54deeb84a4c3db82f684f38071d70bb220ca577f
squiggs <brennan@bell.net>
2022-03-28 20:35:39 -0400

    createindex

---
+++ pages/createindex.js
@@ -0,0 +1,15 @@
+const Createindex = () => {
+    return ( 
+
+        <p>
+            <h3>north america</h3>
+            <h3>south america</h3>
+            <h3>europe</h3>
+            <h3>asia</h3>
+            <h3>africa</h3>
+            <h3>austraila</h3>
+        </p>
+     );
+}
+ 
+export default Createindex;
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,18 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Link from 'next/link'
 
-export default function Home() {
-  return (
-    <div className={styles.container}>
-     <h1>hello world</h1>
-    </div>
-  )
+const Home = () => {
+  return ( 
+
+    <p>
+      <h1>this is home page</h1>
+      <Link href="/createindex"><a>create index</a></Link>
+      
+      <h1>security</h1>
+      <h1>education</h1>
+      <h1>medical</h1>
+      <h1>revenue</h1>
+    </p>
+   );
 }
+ 
+export default Home;
